b166da5458ce76781ae41aaf7bba56023e978f79
Rubens Victor <rubensvictorsantos@gmail.com>
2020-11-09 20:28:05 -0300

    logo copy.svg

--- site/src/components/body/card-prod/cardProd.js
+++ site/src/components/body/card-prod/cardProd.js
@@ -15,13 +15,6 @@ export class CardProd extends Component {
     render() {
         return (
             <div className="card-closed" id="card-prod">
-                <div className="title-card">
-                    <p>
-                        {
-                            this.props.produto.nome_prod
-                        }
-					</p>
-                </div>
                 <picture className="box-img-card">
                     <img src={
                         DOMAIN_IMG + this.props.produto.img_prod
@@ -39,6 +32,13 @@ export class CardProd extends Component {
 
                     </div>
                 </picture>
+                <div className="title-card">
+                    <p>
+                        {
+                            this.props.produto.nome_prod
+                        }
+					</p>
+                </div>
                 <div className="box-price" id="box-price">
                     <p> 
                         {
