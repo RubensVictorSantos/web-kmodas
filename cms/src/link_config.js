b205fba194823aefa197ae7222b25a02cd22c919
Rubens Victor <rubensvictorsantos@gmail.com>
2021-03-19 20:30:59 -0300

    19/03/2021 20:30

--- cms/src/link_config.js
+++ cms/src/link_config.js
@@ -1,3 +1,4 @@
 export const DOMAIN_IMG_DEFAULT = 'http://192.168.1.225:8887/image-not-found.png';
 export const DOMAIN_IMG = 'http://192.168.1.225:8887/';
-export const DOMAIN_API = 'http://192.168.1.224:3333';
+// export const DOMAIN_API = 'http://192.168.1.224:3333';
+export const DOMAIN_API = 'http://localhost:3333';
